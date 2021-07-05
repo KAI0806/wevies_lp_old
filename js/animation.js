cf39e6dc7879d71dfb68c53c8de6b37a844819d8
MASATAKE KAI <nerotan0806@gmail.com>
2021-07-05 14:36:00 +0900

    gitを作成しました。

---
+++ js/animation.js
@@ -0,0 +1,181 @@
+// JavaScript Document
+$(function() {
+	$('.anime1s2').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('a_1s2');
+    } else {
+      $(this).removeClass('a_1s2');
+    }
+		
+	});
+});
+
+$(function() {
+	$('.anime1').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('a_1');
+    } /*else {
+      $(this).removeClass('a_1');
+    }*/
+		
+	});
+});
+
+
+
+$(function() {
+	$('.anime2').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('a_2');
+    }/* else {
+      $(this).removeClass('a_2');
+    }*/
+		
+	});
+});
+
+$(function() {
+	$('.anime3').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('a_3');
+    } /*else {
+      $(this).removeClass('a_3');
+    }*/
+		
+	});
+});
+
+
+$(function() {
+	$('.anime4').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('a_4');
+    } /*else {
+      $(this).removeClass('a_3');
+    }*/
+		
+	});
+});
+
+
+
+$(function() {
+	$('.f_1').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+
+
+$(function() {
+	$('.f_2').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+
+
+$(function() {
+	$('.f_3').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    }/* else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+
+
+$(function() {
+	$('.f_4').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+
+
+$(function() {
+	$('.f_5').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+$(function() {
+	$('.f_6').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+$(function() {
+	$('.f_7').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+$(function() {
+	$('.f_8').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    }/* else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+$(function() {
+	$('.f_9').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+$(function() {
+	$('.f_10').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('f_a1');
+    } /*else {
+      $(this).removeClass('f_a1');
+    }*/
+		
+	});
+});
+
+
+$(function() {
+	$('.anime1').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+		if(isInView){
+			$(this).addClass('a_1');
+    } /*else {
+      $(this).removeClass('a_1');
+    }*/
+		
+	});
+});
